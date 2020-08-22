6cce9deeaa2d12906420eb910b9eaa14450683eb
Kirti Vasdev <kirtivasdev12@gmail.com>
2020-08-22 12:05:55 -0400

    added deployment code to fix error

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -13,7 +15,9 @@ namespace EF_LINQ_OPS
     {
         public static void Main(string[] args)
         {
+            System.IO.Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
